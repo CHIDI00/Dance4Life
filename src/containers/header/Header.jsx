7da83649fdef8d3d29f936f731a812f9d8b9502e
CHIDI MATHAIAS <chidimathayas@gmail.com>
2023-12-09 01:44:42 +0100

    Added Header

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -2,7 +2,26 @@ import React from "react";
 import "./header.css";
 
 const Header = () => {
-	return <div>Header</div>;
+	return (
+		<div className="d4l__header section__margin">
+			<div className="d4l__header-container">
+				<div className="d4l__header-container-1">
+					<div className="d4l__header-container-1_content">
+						<h1>JOIN OUR DANCE CREW</h1>
+						<p>
+							Dance4Life is a dazzling modern theme designed specifically for
+							all types of dance studios, dance classes, dance clubs and
+							enthusiast. Briming with astounding features, powerful elements &
+							astonishing templates
+						</p>
+					</div>
+				</div>
+
+				<div className="d4l__header-container-2"></div>
+				<div className="d4l__header-container-3"></div>
+			</div>
+		</div>
+	);
 };
 
 export default Header;
