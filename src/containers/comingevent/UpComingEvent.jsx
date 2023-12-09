8ba0a9012f1e80ee48a54d93f8457265ca02df5d
CHIDI MATHAIAS <chidimathayas@gmail.com>
2023-12-09 15:28:16 +0100

    Add Event

--- src/containers/comingevent/UpComingEvent.jsx
+++ src/containers/comingevent/UpComingEvent.jsx
@@ -1,9 +1,59 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { RiTicket2Fill } from "react-icons/ri";
 import "./upcomingevent.css";
 
 const UpComingEvent = () => {
+	const [countDown, setCountDown] = useState(345600);
+
+	useEffect(function () {
+		const timeInterval = setInterval(() => {
+			setCountDown((countDownTime) => countDownTime - 1);
+		}, 1000);
+
+		return () => clearInterval(timeInterval);
+	}, []);
+
+	const days = Math.floor(countDown / 86400);
+	const hours = Math.floor((countDown % 86400) / 3600);
+	const minutes = Math.floor((countDown % 3600) / 60);
+	const seconds = (countDown % 3600) % 60;
+
 	return (
-		<div className="d4l__upcomingevent section__padding">upComingEvent</div>
+		<div className="d4l__upcomingevent section__padding">
+			<h1>COUNT EVERY SECONDS UNTIL THE EVENT COMING UP</h1>
+			<div className="d4l__upcomingevent-eventTime_container">
+				{hours !== 0 ? (
+					<>
+						{" "}
+						<div className="event eventTime_day">
+							<h1>{days < 10 ? `0${days}` : days}</h1>
+							<p>DAYS</p>
+						</div>
+						<div className="event eventTime_hour">
+							<h1>{hours}</h1>
+							<p>HOURS</p>
+						</div>
+						<div className="event eventTime_min">
+							<h1>{minutes}</h1>
+							<p>MINUTES</p>
+						</div>
+						<div className="event eventTime_sec">
+							<h1>{seconds}</h1>
+							<p>SECONDS</p>
+						</div>{" "}
+					</>
+				) : (
+					<h1>Ticket sale Closed</h1>
+				)}
+			</div>
+
+			<div className="d4l__upcomingevent-getTicket">
+				<p>Get ticket t access the event</p>
+				<button disabled={hours === 0 ? "true" : "false"}>
+					Get Ticket <RiTicket2Fill />
+				</button>
+			</div>
+		</div>
 	);
 };
 
